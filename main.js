a7bf0189f9e8b3860d1f8084edf7b24672953275
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2022-10-31 18:50:38 +0300

    daily

--- main.js
+++ main.js
@@ -25285,3 +25285,87 @@ compares the values as well as the data types of the operands.
 // // "a,b" == "b,a"
 // console.log(["a", "b"] == "a,b");// true
 
+//--------------------Broadcast Channel API-----------------
+// const APP = {
+//   channel: new BroadcastChannel('wkrp'),
+//   win: null,
+//   init: () => {
+//     //add button listeners
+//     document.getElementById('btnOpen').addEventListener('click', APP.openTab);
+//     document
+//       .getElementById('btnSend')
+//       .addEventListener('click', APP.sendMessage);
+//     //register service worker
+//     navigator.serviceWorker.register('webWorkers/broadsww.js');
+
+//     //listen for broadcasted messages
+//     APP.channel.addEventListener('message', APP.gotMessage);
+//   },
+//   openTab: (ev) => {
+//     //open a new tab / window
+//     APP.win = window.open('./index.html');
+//   },
+//   sendMessage: (ev) => {
+//     let msg = document.getElementById('txtMessage').value;
+//     //send the message on the broadcast channel
+//     if (msg) {
+//       APP.channel.postMessage({ message: msg });
+//     }
+//   },
+//   gotMessage: (ev) => {
+//     //message received on the wkrp broadcast channel
+//     if (ev && ev.data) {
+//       document.getElementById(
+//         'messages'
+//       ).innerHTML += `<p>RECEIVED: ${ev.data.message}</p>`;
+//     }
+//   },
+// };
+
+// //once DOM has loaded as each page loads
+// document.addEventListener('DOMContentLoaded', APP.init);
+
+//-----------------------Tabs and PopUp Windows-------------------
+//script run with defer
+document.getElementById('btnTab').addEventListener('click', this.openTab);
+document.getElementById('btnWin').addEventListener('click', this.openWin);
+
+function openTab(ev) {
+  console.log('open a tab');
+  let win = window.open('html/tab.html', null);
+  win.onload = (ev) => {
+    win.document.body.style.backgroundColor = '#999';
+    setTimeout(() => {
+      win.close();
+    }, 2500);
+  };
+}
+
+function openWin(ev) {
+  console.log('open a popup window');
+  let win = window.open(
+    'html/win.html',
+    null,
+    'popup,width=400,height=400,left=300,top=500, noopener'
+  );
+  // let win = window.open(
+  //   '',
+  //   null,
+  //   'popup,width=400,height=400,left=300,top=500'
+  // );
+  // win.document.write(
+  //   '<html><head><title>Sample</title></head><body>Sample</body></html>'
+  // );
+  // win.onload = () => {
+  // let timmy = setInterval(() => {
+  //   let w = Math.random() * parseInt(window.screen.availWidth);
+  //   let h = Math.random() * parseInt(window.screen.availHeight);
+  //   win.resizeTo(w, h);
+  // }, 1000);
+
+  // setTimeout(() => {
+  //   clearInterval(timmy);
+  //   win.close();
+  // }, 6000);
+  // };
+}
